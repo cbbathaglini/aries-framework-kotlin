1b0e8f57b56562580301c74b50364d71135294af
carine bathaglini <cbbathaglini@gmail.com>
2025-03-27 14:32:26 -0300

    informando mais campos na troca de mensagens

--- ariesframework/src/main/java/org/hyperledger/ariesframework/basicmessage/handlers/BasicMessageHandler.kt
+++ ariesframework/src/main/java/org/hyperledger/ariesframework/basicmessage/handlers/BasicMessageHandler.kt
@@ -6,13 +6,24 @@ import org.hyperledger.ariesframework.agent.Agent
 import org.hyperledger.ariesframework.agent.AgentEvents
 import org.hyperledger.ariesframework.agent.MessageHandler
 import org.hyperledger.ariesframework.basicmessage.messages.BasicMessage
+import org.hyperledger.ariesframework.basicmessage.messages.BasicMessageInfos
 
 class BasicMessageHandler(val agent: Agent) : MessageHandler {
     override val messageType = BasicMessage.type
 
     override suspend fun handle(messageContext: InboundMessageContext): OutboundMessage? {
+
         val message = messageContext.message as BasicMessage
-        agent.eventBus.publish(AgentEvents.BasicMessageEvent(message.content))
+        val connectionRecordId = messageContext.connection?.id
+        val theirLabel = messageContext.connection?.theirLabel
+        val createdAt = messageContext.connection?.createdAt
+        val basicMessageInfos = BasicMessageInfos(
+            content = message.content,
+            connectionRecordId = connectionRecordId,
+            theirLabel = theirLabel,
+            createdAt = createdAt
+        )
+        agent.eventBus.publish(AgentEvents.BasicMessageEvent(basicMessageInfos))
         return null
     }
 }
