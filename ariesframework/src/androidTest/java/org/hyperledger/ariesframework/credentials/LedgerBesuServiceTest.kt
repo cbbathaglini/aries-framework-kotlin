0ea2b03cd4622745ec6762aa15fcabc8fdde7da7
Flavio Caetano Garcia <flavio.garcia@serpro.gov.br>
2025-04-24 09:24:04 -0300

    adicionado comentarios nos testes

--- ariesframework/src/androidTest/java/org/hyperledger/ariesframework/credentials/LedgerBesuServiceTest.kt
+++ ariesframework/src/androidTest/java/org/hyperledger/ariesframework/credentials/LedgerBesuServiceTest.kt
@@ -16,6 +16,22 @@ import kotlin.time.Duration.Companion.seconds
 class LedgerBesuServiceTest {
     lateinit var agent: Agent
 
+
+    /*
+     Run a besu as follows:
+       git clone https://github.com/guilherme-funchal/indy-besu.git
+       cd aries-framework-javascriptindy-besu
+       git checkout Revocation_SERPRO
+       ./network/scripts/run.sh
+
+     Generate data test:
+       cd vdr/wrappers/python
+       python3 -m pip install --upgrade build
+       python3 -m build
+       pip3 install eth_keys
+       python3 -m demo.test
+    */
+
     @Before
     fun setUp() = runTest(timeout = 30.seconds) {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
@@ -28,17 +44,25 @@ class LedgerBesuServiceTest {
     fun tearDown() = runTest {
         agent.reset()
     }
-
+    /*
+    Adjust the data according to those generated in python demo
+     */
     @Test @LargeTest
     fun testBesuSchema() = runTest(timeout = 10.minutes) {
         agent.ledgerService.getSchema("did:ethr:0xce70ce892768d46caf120b600dec29ed20198982/anoncreds/v0/SCHEMA/WZXL9B/1.0.0")
     }
 
+    /*
+    Adjust the data according to those generated in python demo
+     */
     @Test @LargeTest
     fun testBesuCredential() = runTest(timeout = 10.minutes) {
         agent.ledgerService.getCredentialDefinition("did:ethr:0xce70ce892768d46caf120b600dec29ed20198982/anoncreds/v0/CLAIM_DEF/did:ethr:0xce70ce892768d46caf120b600dec29ed20198982:WZXL9B:1.0.0/cred_def_tag")
 
     }
+    /*
+    Adjust the data according to those generated in python demo
+     */
     @Test @LargeTest
     fun testBesuRevocation() = runTest(timeout = 10.minutes) {
         agent.ledgerService.getRevocationRegistryDefinition("did:ethr:0xce70ce892768d46caf120b600dec29ed20198982/anoncreds/v0/REV_REG_DEF/did:ethr:0xce70ce892768d46caf120b600dec29ed20198982:WZXL9B:1.0.0/cred_def_tag/rev_reg_def_tag")
