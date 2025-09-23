7e1ccad623c57031d22863982caf6f7b8bcf15da
Carine Bertagnolli Bathaglini <cbbathaglini@MacBook-Pro-de-Carine.local>
2025-09-23 15:10:06 -0300

    removing import

--- ariesframework/src/main/java/org/hyperledger/ariesframework/anoncreds/model/AnonCredsSchema.kt
+++ ariesframework/src/main/java/org/hyperledger/ariesframework/anoncreds/model/AnonCredsSchema.kt
@@ -1,6 +1,5 @@
 package org.hyperledger.ariesframework.anoncreds.model
 
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
